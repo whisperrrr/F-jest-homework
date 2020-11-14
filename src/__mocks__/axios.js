508378bfb7bf869f74027c38a5c4cedc47cf2d56
whisperrrr <jh731448520@163.com>
2020-11-14 22:45:37 +0800

    [Jin Hui][feat] - finish async.mock class (TODO 19--20)

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -2,4 +2,5 @@ import { jest } from "@jest/globals";
 
 export default {
   get: jest.fn(() => Promise.resolve({ data: { name: "Hebe Tien" } })),
+  post: jest.fn(() => Promise.resolve({ data: { username: "Hebe Tien" } })),
 };
--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,23 @@
 import { register } from "../user";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    await expect(register("Hebe", "330")).resolves.toEqual({
+      username: "Hebe Tien",
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyPassword.mockReturnValue(false);
+
+    await expect(register("Hebe", "330")).rejects.toThrow(
+      "wrong username or password"
+    );
   });
 });
